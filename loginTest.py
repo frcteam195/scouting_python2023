3eaaf6f17114f1565f71a0511294f2f745f460e6
matt <matthewjagos@gmail.com>
2022-05-23 20:33:34 -0400

    db login

---
+++ loginTest.py
@@ -0,0 +1,15 @@
+import configparser
+import dbLogin as p
+import argparse
+import sys
+c = configparser.ConfigParser()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-db", "--database", help = "Choices: aws-prod, aws-dev", required= True)
+args = parser.parse_args()
+input_database = args.database
+
+p.login(input_database)
+
+
+print(p.getdict())
