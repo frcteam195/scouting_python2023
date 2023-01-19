c66e1b99001a386618b40e09ef647c897965c613
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-19 12:33:33 -0500

    added temp table to hold level2 scouting data for the app

---
+++ SQL/dev1/tmpLevel2-01.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS dev1.tmpLevel2 (
+        BAeventID VARCHAR(20) NOT NULL,
+        matchID INT NOT NULL,
+        matchNum INT NULL,
+        scouterID INT NULL,
+        scoutingStatus INT NULL,
+        synced2MS BOOLEAN DEFAULT false,
+        team VARCHAR(10) NOT NULL,
+        teamMatchNum INT NULL,
+        scoutingRoleID INT NULL,
+        /* add Level 2 columns here. Note that these columns must match those of the level2 DB table */
+        PRIMARY KEY (matchID),
+        FOREIGN KEY (BAeventID) REFERENCES events (BAeventID) ON DELETE CASCADE,
+        FOREIGN KEY (matchID) REFERENCES matches (id) ON DELETE CASCADE,
+        FOREIGN KEY (scouterID) REFERENCES scouters (id) ON DELETE CASCADE,
+        FOREIGN KEY (team) REFERENCES CEteams (team) ON DELETE CASCADE,
+        FOREIGN KEY (scoutingRoleID) REFERENCES scoutingRole (id) ON DELETE CASCADE
+) Engine = InnoDB;
