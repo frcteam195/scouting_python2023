d578c7a7d3362df0b35b84de709c1894da29e55a
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-20 14:59:34 -0500

    first Python script for 2023, eventsAll script

---
+++ BA/eventsAll.py
@@ -0,0 +1,133 @@
+# Python3 script that pulls all FRC registered teams for the current year and
+#	writes the full team list to the teamsAll table in the Team 195 DB
+# Script is intended to be run once at the beginning of the season
+
+import mariadb as mariaDB
+import tbapy
+import datetime
+import re
+import sys
+import argparse
+
+tba = tbapy.TBA('Tfr7kbOvWrw0kpnVp5OjeY780ANkzVMyQBZ23xiITUkFo9hWqzOuZVlL3Uy6mLrz')
+currentYear = datetime.datetime.today().year
+database = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-db", "--database", help = "Choices: aws-dev1, aws-dev2, pi-192, pi-10, localhost", required=True)
+args = parser.parse_args()
+input_database = args.database
+
+if input_database == "aws-dev1":
+    database = "aws-dev1"
+elif input_database == "aws-dev2":
+    database = "aws-dev2"
+elif input_database == "pi-192":
+    database = "pi-192"
+elif input_database == "pi-10":
+    database = "pi-10"
+elif input_database == "localhost":
+    database = "localhost"
+else:
+    print(input_database + " is not a invalid database choice. See --help for choices")
+    sys.exit()
+
+print ("Connecting to " + database)
+
+def wipeBAE():
+        cursor.execute("DELETE FROM eventsAll;")
+        cursor.execute("ALTER TABLE eventsAll AUTO_INCREMENT = 1;")
+        conn.commit()
+      
+
+def onlyascii(s):
+    return "".join(i for i in s if ord(i) < 128 and ord(i) != 39)
+if database == "aws-dev1":
+            print("Input database " + input_database)
+            conn = mariaDB.connect(user='',
+                                       passwd='',
+                                        host='',
+                                       database='dev1')
+            cursor = conn.cursor()
+        
+elif database == "pi-10":
+            conn = mariaDB.connect(user='',
+                                passwd='',
+                                host='10.0.20.195',
+                                database='team195_scouting')
+            cursor = conn.cursor()
+            
+elif database == "pi-192":
+            conn = mariaDB.connect(user='',
+                                passwd='',
+                                host='192.168.1.195',
+                                database='team195_scouting')
+            cursor = conn.cursor()
+
+elif database == "localhost":
+        conn = mariaDB.connect(user='',
+                                passwd='',
+                                host='localhost',
+                                database='team195_scouting')
+        cursor = conn.cursor()
+
+elif database == "aws-prod":
+        conn = mariaDB.connect(user='',
+                                passwd='',
+                                host='',
+                                database='team195_scouting')
+        cursor = conn.cursor()
+
+else: 
+        print ("no love for Harish!")
+        sys.exit()
+
+wipeBAE() 
+
+totalEvents = tba.events(year=currentYear)
+eventList = []
+
+for event in totalEvents:
+    eventCode = event.get('event_code')
+    eventName = event.get('short_name')
+    eventWeek = event.get('week')
+    eventCity = event.get('city')
+    eventStateProv = event.get('state_prov')
+    eventCountry = event.get('country')
+    eventLocation = (eventCity + ", " + eventStateProv + ", " + eventCountry)
+    eventStartDate = event.get('start_date')
+    eventEndDate = event.get('end_date')
+    BAEventID = event.get('key')
+    
+    print (eventCode + " " + eventLocation)
+    
+    eventName = onlyascii(eventName)
+    eventLocation = onlyascii(eventLocation)
+
+    if len(eventName) > 50:
+        eventName = eventName[:40]
+    if eventName is None:
+    	eventName = "no name"
+    eventName = re.sub("[{}]","", eventName)
+    eventName = re.sub("[()]","", eventName)
+    eventLocation = eventLocation.replace("'","")
+    if len(eventCity) > 50:
+    	eventCity = eventCity[:40]
+    if len(eventCountry) > 50:
+    	eventCountry = eventCountry[:40]
+    if eventWeek is None:
+    	eventWeek = 8
+    
+    
+    query = "INSERT INTO eventsAll (eventCode, eventName, eventWeek, eventLocation, eventStartDate, eventEndDate, BAEventID) VALUES " + \
+            "('" + str(eventCode) + \
+            "','" + str(eventName) + \
+            "','" + str(eventWeek) + \
+            "','" + str(eventLocation) + \
+            "','" + str(eventStartDate) + \
+            "','" + str(eventEndDate) + \
+            "','" + str(BAEventID) + "');"
+    print(query)
+    
+    cursor.execute(query)
+    conn.commit()
