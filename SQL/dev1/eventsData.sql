310d7b7a9e0233258a7661fa9ad05592f4fc12d8
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-22 17:03:30 -0500

    sdf

--- SQL/dev1/eventsData.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `events` VALUES 
-(1,'2023cthar',0,'Hartford','East Hartford, CT, USA','2023-03-31','2023-04-02'),
-(2,'2023mawne',0,'Western NE','Springfield, MA, USA','2023-03-16','2023-03-18')
-;
