da2dec0bc3e86f5e49ea178035ea2b17b3c258ac
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-23 13:45:18 -0500

    changed scotingRoles to allianceStations

---
+++ SQL/dev1/allianceStations-01.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS dev1.allianceStations (
+        id INT AUTO_INCREMENT NOT NULL,
+        allianceStation VARCHAR(20),
+        PRIMARY KEY (id)
+) Engine = InnoDB;
--- SQL/dev1/scoutingRoles-01.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS dev1.scoutingRoles (
-        id INT AUTO_INCREMENT NOT NULL,
-        scoutingRole VARCHAR(25),
-        PRIMARY KEY (id)
-) Engine = InnoDB;
