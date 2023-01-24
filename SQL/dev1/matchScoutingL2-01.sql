ff236d037ff309a45b87042568407589b2c3bf1d
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-24 17:04:39 -0500

    reorder columns in match scouting table and fixed bugs in L2 table

--- SQL/dev1/matchScoutingL2-01.sql
+++ SQL/dev1/matchScoutingL2-01.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS dev1.level2 (
+CREATE TABLE IF NOT EXISTS dev1.matchScoutingL2 (
+        id INT AUTO_INCREMENT NOT NULL,
         eventID INT NOT NULL,
         matchID INT NOT NULL,
         matchNum INT NULL,
-        scouterID INT NULL,
-        scoutingStatus INT NULL,
-        synced2MS BOOLEAN DEFAULT false,
+        allianceStationID INT NULL,
         team VARCHAR(10) NOT NULL,
         teamMatchNum INT NULL,
-        allianceStationID INT NULL,
+        scoutingStatus INT NULL,
+        scouterID INT NULL,
+        synced2MS BOOLEAN DEFAULT false,
         /* add Level 2 columns here. Note that these columns must match those of the matchScouting DB table */
-        PRIMARY KEY (matchID),
+        PRIMARY KEY (id),
         FOREIGN KEY (eventID) REFERENCES events (id),
         FOREIGN KEY (matchID) REFERENCES matches (id),
         FOREIGN KEY (scouterID) REFERENCES scouters (id),
