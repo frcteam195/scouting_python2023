4886da5dbdcb075fb10081f94eda0be3d68bbe45
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-25 16:25:23 -0500

    scripts to look for differences in SQL file directories

--- SQL/dev1/rename-db.sh
+++ SQL/dev1/rename-db.sh
@@ -1,11 +1,13 @@
 # script to rename the database in the SQL create table files
+db1=$1
+db2=$2
 
 if [[ $# != 2 ]]; then
     echo 'you must enter the starting and ending database name'
     echo 'e.g. ./rename-db.sh dev1 dev2'
     exit 0
 else
-    find . -type f -name '*.sql' -exec sed -i 's/dev1/dev2/' {} \;
+    find . -type f -name '*.sql' -exec sed -i "s/$db1/$db2/" {} \;
     echo ''
     echo 'removing and rebuilding all.sql with build.sh'
     echo ''
