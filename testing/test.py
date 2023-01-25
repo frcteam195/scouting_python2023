298b1bd04d98e615382e8d6cc110634d84229626
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-01-24 22:06:21 -0500

    added building of matchScoutingL2 records to the createMatchScoutingRecords script

--- testing/test.py
+++ testing/test.py
@@ -1,24 +1,6 @@
-import configparser
-import mariadb as mariaDB
-
-# Read the configuration file
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-# Get the database login information from the configuration file
-host = config['aws-dev1']['host']
-user = config['aws-dev1']['user']
-passwd = config['aws-dev1']['passwd']
-database = config['aws-dev1']['database']
-
-print (host + " " + user + " " + passwd + " " + database)
-
-conn = mariaDB.connect(user=user, passwd=passwd, host=host, database=database)
-cursor = conn.cursor()
-
-query = "select BAeventID from eventsAll;"
-print (query)
-
-cursor.execute(query)
-results = cursor.fetchall()
-print (results)
+tables = ['matchScouting', 'matchScoutingL2']
+for table in tables:
+    query = "SELECT " + table + ".id FROM " + table + " INNER JOIN events " + \
+            "ON " + table + ".eventID = events.id AND ((events.currentEvent) = 1) " + \
+            "WHERE " + table + ".team = " + table + " ORDER BY " + table + ".id;"
+    print(query)
