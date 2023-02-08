23f33fdaa9c80b160c4c900633bc392c6c2dec7f
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-02-08 17:17:16 -0500

    cleaning up code

--- analysisTypes/autoGamePieces.py
+++ analysisTypes/autoGamePieces.py
@@ -6,15 +6,13 @@ def autoGamePieces(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitDat
     rsCEA['analysisTypeID'] = 3
     numberOfMatchesPlayed = 0
 
-    # only using to test ranks, eliminate later
     autoGamePiecesList = []
 
     # Loop through each match the robot played in.
     for matchResults in rsRobotMatchData:
         rsCEA['team'] = matchResults[analysis.columns.index('team')]
         rsCEA['eventID'] = matchResults[analysis.columns.index('eventID')]
-        # We are hijacking the starting position to write DNS or UR. This should go to Auto as it will not
-        #   likely be displayed on team picker pages.
+        
         preNoShow = matchResults[analysis.columns.index('preNoShow')]
         scoutingStatus = matchResults[analysis.columns.index('scoutingStatus')]
         if preNoShow == 1:
@@ -22,44 +20,43 @@ def autoGamePieces(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitDat
         elif scoutingStatus == 2:
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = 'UR'
         else:
-            
-            
             numGamePieces = 0
-            score1 = matchResults[analysis.columns.index('autoScore1')]
-            score2 = matchResults[analysis.columns.index('autoScore2')]
-            score3 = matchResults[analysis.columns.index('autoScore3')]
-            score4 = matchResults[analysis.columns.index('autoScore4')]
+            autoScorePos1 = matchResults[analysis.columns.index('autoScorePos1')]
+            autoScorePos2 = matchResults[analysis.columns.index('autoScorePos2')]
+            autoScorePos3 = matchResults[analysis.columns.index('autoScorePos3')]
+            autoScorePos4 = matchResults[analysis.columns.index('autoScorePos4')]
 
-            if score1 > 0:
+            if autoScorePos1 > 0:
                 numGamePieces += 1
-            if score2 > 0:
+            if autoScorePos2 > 0:
                 numGamePieces += 1
-            if score3 > 0:
+            if autoScorePos3 > 0:
                 numGamePieces += 1
-            if score4 > 0:
+            if autoScorePos4 > 0:
                 numGamePieces += 1
 
-            autoDisplay = numGamePieces
-            autoValue = numGamePieces
+            autoGamePiecesDisplay = numGamePieces
+            autoGamePiecesValue = numGamePieces
+            autoGamePiecesList.append(autoGamePiecesValue)
+            numberOfMatchesPlayed += 1
 
             if numGamePieces == 0:
-                autoColor = 1
+                autoGamePiecesColor = 1
             elif numGamePieces == 1:
-                autoColor = 2
+                autoGamePiecesColor = 2
             elif numGamePieces == 2:
-                autoColor =3
+                autoGamePiecesColor =3
             elif numGamePieces == 3:
-                autoColor = 4
+                autoGamePiecesColor = 4
             elif numGamePieces > 3:
-                autoColor = 5
+                autoGamePiecesColor = 5
+            else:
+                print("number of game pieces appear to be negative. Aborting!")
+                quit()
 
-            # Increment the number of matches played and write M#D, M#V and M#F
-            numberOfMatchesPlayed += 1
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = autoDisplay
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'V'] = autoValue
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'F'] = autoColor
-
-            autoGamePiecesList.append(autoValue)
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = str(autoGamePiecesDisplay)
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'V'] = autoGamePiecesValue
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'F'] = autoGamePiecesColor
 
     if numberOfMatchesPlayed > 0:
         rsCEA['S1V'] = round(statistics.mean(autoGamePiecesList), 1)
