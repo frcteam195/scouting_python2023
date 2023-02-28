53229673c85ee14650d7e23f9dc29a0d409708b5
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-02-27 20:19:47 -0500

    sadf

--- analysis.sh
+++ analysis.sh
@@ -1,6 +1,6 @@
 mkdir -p /home/team195/DBbackups
-now=$(date +%Y-%m-%d_%H-%M)
+now=$(/bin/date +%Y-%m-%d_%H-%M)
 cd /home/team195/DBbackups
-/usr/bin/mysql -h localhost dev1 > event_"$now".sql
+/usr/bin/mysqldump -h localhost dev1 > event_"$now".sql
 /bin/tar -czf event_"$now".tgz event_"$now".sql
 /bin/rm -f event_"$now".sql
