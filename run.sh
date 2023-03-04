6d737933f4c144ed2a37494caeb7521dcc1147aa
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-04 16:16:12 -0500

    asdf

--- run.sh
+++ run.sh
@@ -26,13 +26,13 @@ python "$path"/copyBAmatchData.py -db "$db" -host "$host"
 echo ''; echo '***** Running insertFoulsRP.py *****'
 python "$path"/insertFoulsRP.py -db "$db" -host "$host"
 
-# echo ''; echo '***** Running syncTable to migrate matchScoutingL2 from aws to localhost *****'
-# python "$path"/syncTable.py -db1 "$remoteDB" -host1 "$remoteHost" \
-#   -db2 "$db" -host2 "$host" -table matchScoutingL2 -id matchScoutingL2ID
+echo ''; echo '***** Running syncTable to migrate matchScoutingL2 from aws to localhost *****'
+python "$path"/syncTable.py -db1 "$remoteDB" -host1 "$remoteHost" \
+  -db2 "$db" -host2 "$host" -table matchScoutingL2 -id matchScoutingL2ID
 
-# echo ''; echo '***** Running syncTable to migrate pit from aws to localhost *****'
-# python "$path"/syncTable.py -db1 "$remoteDB" -host1 "$remoteHost" \
-#   -db2 "$db" -host2 "$host" -table pit -id pitID
+echo ''; echo '***** Running syncTable to migrate pit from aws to localhost *****'
+python "$path"/syncTable.py -db1 "$remoteDB" -host1 "$remoteHost" \
+  -db2 "$db" -host2 "$host" -table pit -id pitID
 
 echo ''; echo '***** Running analysisIR.py *****'
 python "$path"/analysisIR.py -db "$db" -host "$host"
@@ -49,27 +49,27 @@ python "$path"/syncTable.py -db1 "$db" -host1 "$host" \
   -db2 "$remoteDB" -host2 "$remoteHost" -table matches -id matchID
 
 # mysqldump / mysql for data sync between localhost and aws
-# mysqldump "db" CEanalysis > dump.sql
-# mysqldump "db" CEanalysisGraphs >> dump.sql
-# mysqldump "db" dnpList >> dump.sql
-# mysqldump "db" final24 >> dump.sql
-# mysqldump "db" pickList1 >> dump.sql
-# mysqldump "db" watch1 >> dump.sql
-# mysqldump "db" watch2 >> dump.sql
-# mysql -h scouting.team195.com "remoteDB"  < dump.sql
-# rm -f dump.sql
+mysqldump "db" CEanalysis > dump.sql
+mysqldump "db" CEanalysisGraphs >> dump.sql
+mysqldump "db" dnpList >> dump.sql
+mysqldump "db" final24 >> dump.sql
+mysqldump "db" pickList1 >> dump.sql
+mysqldump "db" watch1 >> dump.sql
+mysqldump "db" watch2 >> dump.sql
+mysql -h scouting.team195.com "remoteDB"  < dump.sql
+rm -f dump.sql
 
 # share script for sharing L1 quantitative data
 echo ''; echo '***** running share.py *****'
 python "$path"/share.py -db "$db" -host "$host"
 
 # DB backup
-# mkdir -p /home/team195/DBbackups
-# now=$(/bin/date +%Y-%m-%d_%H-%M)
-# cd /home/team195/DBbackups
-# /usr/bin/mysqldump -h localhost "$db" > event_"$now".sql
-# /bin/tar -czf event_"$now".tgz event_"$now".sql
-# /bin/rm -f event_"$now".sql
+mkdir -p /home/team195/DBbackups
+now=$(/bin/date +%Y-%m-%d_%H-%M)
+cd /home/team195/DBbackups
+/usr/bin/mysqldump -h localhost "$db" > event_"$now".sql
+/bin/tar -czf event_"$now".tgz event_"$now".sql
+/bin/rm -f event_"$now".sql
 
 /usr/bin/scp -i /home/team195/scouting.pem "$path"/195scoutingData.csv ubuntu@scouting.team195.com:~
 /usr/bin/scp -i /home/team195/scouting.pem "$path"/195scoutingData.json ubuntu@scouting.team195.com:~
