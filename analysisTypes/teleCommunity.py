683253d21b31d1c4a9759ad22e060fb37975ad9e
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-08 21:02:24 -0500

    sdf

--- analysisTypes/teleCommunity.py
+++ analysisTypes/teleCommunity.py
@@ -5,16 +5,11 @@ def teleCommunity(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitData
     rsCEA = {}
     rsCEA['analysisTypeID'] = 11
     numberOfMatchesPlayed = 0
-
-    # only using to test ranks, eliminate later
     teleCommunityList = []
     
-    # Loop through each match the robot played in.
     for matchResults in rsRobotMatchData:
         rsCEA['team'] = matchResults[analysis.columns.index('team')]
         rsCEA['eventID'] = matchResults[analysis.columns.index('eventID')]
-        # We are hijacking the starting position to write DNS or UR. This should go to Auto as it will not
-        #   likely be displayed on team picker pages.
         preNoShow = matchResults[analysis.columns.index('preNoShow')]
         scoutingStatus = matchResults[analysis.columns.index('scoutingStatus')]
         if preNoShow == 1:
@@ -22,8 +17,6 @@ def teleCommunity(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitData
         elif scoutingStatus == 2:
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = 'UR'
         else:
-            
-            
             coneCMTY = matchResults[analysis.columns.index('teleConeCMTY')]
             cubeCMTY = matchResults[analysis.columns.index('teleCubeCMTY')]
             LZPickup = matchResults[analysis.columns.index('teleLZPickup')]
@@ -35,31 +28,30 @@ def teleCommunity(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitData
             if LZPickup is None:
                 LZPickup = 0
 
-
             totalCMTY = coneCMTY + cubeCMTY
 
-            teleCommunityDisplay = str(totalCMTY) + "|" + str(LZPickup)
-            teleCommunityValue = totalCMTY
-            if totalCMTY is None:
-                teleCommunityColor = 1
-            elif totalCMTY == 0:
-                teleCommunityColor = 1
-            elif totalCMTY <= 3:
-                teleCommunityColor = 2
-            elif totalCMTY <= 6:
-                teleCommunityColor = 3
-            elif totalCMTY <= 9:
-                teleCommunityColor = 4
+            display = str(totalCMTY) + "|" + str(LZPickup)
+            value = totalCMTY
+            if LZPickup == 0:
+                color = 1
+            elif LZPickup <= 3:
+                color = 2
+            elif LZPickup <= 6:
+                color = 3
+            elif LZPickup <= 9:
+                color = 4
+            elif LZPickup > 9:
+                color = 5
             else:
-                teleCommunityColor = 5
+                print('teleCommunity: that should not happen')
+                quit()
 
-            # Increment the number of matches played and write M#D, M#V and M#F
             numberOfMatchesPlayed += 1
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = teleCommunityDisplay
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'V'] = teleCommunityValue
-            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'F'] = teleCommunityColor
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = display
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'V'] = value
+            rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'F'] = color
 
-            teleCommunityList.append(teleCommunityValue)
+            teleCommunityList.append(value)
 
     if numberOfMatchesPlayed > 0:
         rsCEA['S1V'] = round(statistics.mean(teleCommunityList), 1)
