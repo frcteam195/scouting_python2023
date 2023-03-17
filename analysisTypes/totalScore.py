b0c9405195edcd38a956d6a8a47f76424ba99f33
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-17 16:47:48 -0400

    sdf

--- analysisTypes/totalScore.py
+++ analysisTypes/totalScore.py
@@ -19,67 +19,78 @@ def totalScore(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitData):
         elif scoutingStatus == 2:
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = 'UR'
         else:
+            autoMB = matchResults[analysis.columns.index('autoMB')]
+            scorePos1 = matchResults[analysis.columns.index('autoScore1')]
+            scorePos2 = matchResults[analysis.columns.index('autoScore2')]
+            scorePos3 = matchResults[analysis.columns.index('autoScore3')]
+            scorePos4 = matchResults[analysis.columns.index('autoScore4')]
+            autoRamp = matchResults[analysis.columns.index('autoRamp')]
+            
             teleconeHigh = matchResults[analysis.columns.index('teleConeHigh')]
             teleconeMid = matchResults[analysis.columns.index('teleConeMid')]
             teleconeLow = matchResults[analysis.columns.index('teleConeLow')]
-
             telecubeHigh = matchResults[analysis.columns.index('teleCubeHigh')]
             telecubeMid = matchResults[analysis.columns.index('teleCubeMid')]
             telecubeLow = matchResults[analysis.columns.index('teleCubeLow')]
 
             ramp = matchResults[analysis.columns.index('ramp')]
 
-            autoRamp = matchResults[analysis.columns.index('autoRamp')]
-            autoRampDisplay = ""
-
-            scorePos1 = matchResults[analysis.columns.index('autoScore1')]
-            scorePos2 = matchResults[analysis.columns.index('autoScore2')]
-            scorePos3 = matchResults[analysis.columns.index('autoScore3')]
-            scorePos4 = matchResults[analysis.columns.index('autoScore4')]
+            # auto
+            if autoMB == 2:
+                moveBonusPts = 3
+            else:
+                moveBonusPts = 0
             
-            autoScore = 0
-            autoMB = matchResults[analysis.columns.index('autoMB')]
-
-            if autoMB is None:
-                autoMB = 0
-
+            autoGPscore = 0
             if (scorePos1 >= 1) and (scorePos1 <= 9):
-                autoScore += 6
+                autoGPscore += 6
             elif (scorePos1 >= 10) and (scorePos1 <= 18):
-                autoScore += 4
+                autoGPscore += 4
             elif (scorePos1 >= 19) and (scorePos1 <= 27):
-                autoScore += 3
+                autoGPscore += 3
             
             if (scorePos2 >= 1) and (scorePos2 <= 9):
-                autoScore += 6
+                autoGPscore += 6
             elif (scorePos2 >= 10) and (scorePos2 <= 18):
-                autoScore += 4
+                autoGPscore += 4
             elif (scorePos2 >= 19) and (scorePos2 <= 27):
-                autoScore += 3
+                autoGPscore += 3
 
             if (scorePos3 >= 1) and (scorePos3 <= 9):
-                autoScore += 6
+                autoGPscore += 6
             elif (scorePos3 >= 10) and (scorePos3 <= 18):
-                autoScore += 4
+                autoGPscore += 4
             elif (scorePos3 >= 19) and (scorePos3 <= 27):
-                autoScore += 3
+                autoGPscore += 3
 
             if (scorePos4 >= 1) and (scorePos4 <= 9):
-                autoScore += 6
+                autoGPscore += 6
             elif (scorePos4 >= 10) and (scorePos4 <= 18):
-                autoScore += 4
+                autoGPscore += 4
             elif (scorePos4 >= 19) and (scorePos4 <= 27):
-                autoScore += 3
+                autoGPscore += 3
             
-            autoScore += autoMB
+            if autoRamp is None:
+                autoRampDisplay = ""
+                autoRampPts = 0
+            elif autoRamp == 5:
+                autoRampDisplay = "*"
+                autoRampPts = 12
+            elif autoRamp == 4:
+                autoRampDisplay = "*"
+                autoRampPts = 8
+            else:
+                autoRampDisplay = ""
 
+            autoScore = autoGPscore + moveBonusPts + autoRampPts
+
+            # teleop
             if teleconeHigh is None:
                 teleconeHigh = 0
             if teleconeMid is None:
                 teleconeMid = 0
             if teleconeLow is None:
                 teleconeLow = 0
-
             if telecubeHigh is None:
                 telecubeHigh = 0
             if telecubeMid is None:
@@ -87,43 +98,43 @@ def totalScore(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitData):
             if telecubeLow is None:
                 telecubeLow = 0
 
-            if ramp is None:
-                ramp = 0
-            
-            if autoRamp is None:
-                autoRamp = 0
-                autoRampDisplay = ""
-            elif autoRamp > 0:
-                autoRampDisplay = "*"
-            else:
-                autoRampDisplay = ""
-                
             teleHigh = teleconeHigh + telecubeHigh
             teleMid = teleconeMid + telecubeMid
             teleLow = teleconeLow + telecubeLow
 
-            total = 0
+            linkPts = (teleHigh + teleMid + teleLow) / 3
+
+            telePts = (teleHigh * 5) + (teleMid * 3) + (teleLow * 2) + linkPts
+
+            # end-game
+            if ramp == 5:
+                rampPts = 10
+            elif ramp == 4:
+                rampPts = 6
+            elif ramp == 3:
+                rampPts = 2
+            elif ramp == 2:
+                rampPts = 2
+            else:
+                rampPts = 0
             
-            link = int((teleHigh + teleLow + teleMid) / 3)
+            totalScore = 0
+            totalScore = autoScore + telePts + rampPts + autoRamp
 
-            total += autoScore + (teleHigh * 5) + (teleMid * 3) + (teleLow * 2) + link + ramp + autoRamp
+            totalScoreDisplay = str(totalScore) + autoRampDisplay
+            totalScoreValue = totalScore
 
-            totalScoreDisplay = str(total) + autoRampDisplay
-            totalScoreValue = total
-
-            if total <=15:
+            if totalScore <=15:
                 totalScoreColor = 1
-            elif total <=30:
+            elif totalScore <=30:
                 totalScoreColor = 2
-            elif total <=40:
+            elif totalScore <=40:
                 totalScoreColor = 3
-            elif total <=50:
+            elif totalScore <=50:
                 totalScoreColor = 4
-            elif total > 50:
+            elif totalScore > 50:
                 totalScoreColor = 5
 
-
-            # Increment the number of matches played and write M#D, M#V and M#F
             numberOfMatchesPlayed += 1
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = totalScoreDisplay
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'V'] = totalScoreValue
