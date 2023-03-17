afd6e586d947ced371aedf52096f172a155115cf
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-17 10:08:27 -0400

    sdf

--- run-loop.sh
+++ run-loop.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 i=1
-while [ $i -le 5 ]
+while [ $i -le 500 ]
 do
   echo "************************************************************"
   echo "Loop $i"
-  ./run.sh
+  ./run.aws.sh
   i=$(( $i + 1 ))
+  sleep 60
 done
