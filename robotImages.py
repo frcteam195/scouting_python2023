ccad4df5c85ca89c9b99c9132735c49cb0c5918d
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-17 09:32:21 -0400

    sdf

--- robotImages.py
+++ robotImages.py
@@ -27,8 +27,9 @@ for jpeg_file in jpeg_files:
         image = image.rotate(-90)
         width = image.width
         height = image.height
-        # new_width = int((height / width) * max_size[0])
-        image.thumbnail((max_size[0], 1000))
+        new_height = int((height / width) * max_size[0])
+        # image.thumbnail((max_size[0], 1000))
+        image.thumbnail((max_size[0], new_height))
         save = f"{directory}/robotThumbnails/{filename}"
         image.save(save, quality=quality)
         processed_images.append(filename)
