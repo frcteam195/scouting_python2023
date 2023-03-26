37119314101cc0ddb1ca3d0178abc9cdb401c9a9
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-03-26 18:12:12 -0400

    SDf

--- run.sh
+++ run.sh
@@ -48,6 +48,9 @@ python "$path"/analysisIR.py -db "$db" -host "$host"
 echo ''; echo '***** Running graphIR.py *****'
 python "$path"/graphIR.py -db "$db" -host "$host"
 
+echo ''; echo '***** Running scorePredict.py *****'
+python "$path"/scorePredict.py -db "$db" -host "$host"
+
 echo ''; echo '***** Running syncTable to migrate matchScouting from localhost to aws *****'
 python "$path"/syncTable.py -db1 "$db" -host1 "$host" \
   -db2 "$remoteDB" -host2 "$remoteHost" -table matchScouting -id matchScoutingID
