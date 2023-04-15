9838a34239c5cd4a84358ba4372f2789b7cc6703
ddrex <44214161+ddrex@users.noreply.github.com>
2023-04-15 15:22:41 -0400

    Added Brake Mode Type to Pit and created brakeModeTypes table

---
+++ SQL/brakeModeTypes-01.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS dev1.brakeModeTypes (
+        brakeModeTypeID INT NOT NULL,
+        brakeModeType VARCHAR(20),
+        PRIMARY KEY (brakeModeTypeID)
+) Engine = InnoDB;
