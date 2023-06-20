db72f70f9d85d0386866b98e75eeba7b84cac6ef
Mark Maciejewski <mark.maciejewski@gmail.com>
2023-06-19 20:18:15 -0400

    sdf

--- analysisTypes/postShelfPickup.py
+++ analysisTypes/postShelfPickup.py
@@ -43,6 +43,8 @@ def postShelfPickup(analysis, rsRobotMatchData, rsRobotL2MatchData, rsRobotPitDa
             else:
                 postShelfPickupColor = 5
 
+            postShelfPickupList.append(postShelfPickupList)
+
             # Increment the number of matches played and write M#D, M#V and M#F
             numberOfMatchesPlayed += 1
             rsCEA['M' + str(matchResults[analysis.columns.index('teamMatchNum')]) + 'D'] = postShelfPickupDisplay
